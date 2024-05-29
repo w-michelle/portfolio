890b3cf779da3df0f95529f66cb9f5e2dfa8f5ab
w-michelle <89858197+w-michelle@users.noreply.github.com>
2024-05-29 15:30:39 -0400

    edit layout

--- app/page.js
+++ app/page.js
@@ -1,10 +1,6 @@
-import Image from "next/image";
-import { Inter, JetBrains_Mono } from "next/font/google";
 import Landing from "./components/landing";
 import Projects from "./components/projects";
 import Contact from "./components/contact";
-import Link from "next/link";
-import About from "./components/about";
 
 export default function Home() {
   return (
