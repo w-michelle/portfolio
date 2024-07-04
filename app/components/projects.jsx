910bab086e890a72ec50a2c80ac59ec426338cff
w-michelle <89858197+w-michelle@users.noreply.github.com>
2024-07-04 12:57:50 -0400

    Edit projects

--- app/components/projects.jsx
+++ app/components/projects.jsx
@@ -9,18 +9,10 @@ const jetbrains = JetBrains_Mono({
   });
 
   const projectInfo = [
- {
-        title:"CYBER BUBBLE",
-        desc: "First project, inspired by imissmybar with added organizational tools",
-        stacks: "[React/TailwindCSS/Firebase]",
-        imgSrc: "/cb-home.png",
-        siteUrl: "https://cyberbubblev2.vercel.app",
-        github: "https://github.com/w-michelle/cyberbubblev2"
 
-    },
     {
         title: "START FROM ZERO",
-        desc: "Custom wood-work e-commerce *Note: To test payment, use card number:[4242 4242 4242 4242]. ",
+        desc: "Custom wood-work e-commerce *Note: To test payment, use:[4242 4242 4242 4242]. ",
         stacks: "[Typescript/Redux/Next.js/TailwindCSS/Prisma/PostgreSQL/Stripe/AWS S3/NextAuth]",
         imgSrc:"/sfz.png",
         siteUrl: "https://startfromzero.vercel.app/",
@@ -34,6 +26,14 @@ const jetbrains = JetBrains_Mono({
         siteUrl: "https://roamwith.vercel.app/",
         github: "https://github.com/w-michelle/roam"
     },
+    {
+        title: "CBXWORLD MEMBER'S ONLY CLUB",
+        desc: "Member's only clubhouse where only members can write anonymous posts",
+        stacks: "[React/Typescript/TailwindCSS/Express.js/Node.js/MongoDB/Redux/JWT]",
+        imgSrc:"/cbxworld.png",
+        siteUrl: "https://cbxworld.vercel.app/",
+        github:"https://github.com/w-michelle/cbxworld"
+    },
 
     {
         title: "TRAVELGRAM",
@@ -44,13 +44,15 @@ const jetbrains = JetBrains_Mono({
         github:"https://github.com/w-michelle/travelgram"
     },
     {
-        title: "CBXWORLD MEMBER'S ONLY CLUB",
-        desc: "Member's only clubhouse where only members can write anonymous posts",
-        stacks: "[React/Typescript/TailwindCSS/Express.js/Node.js/MongoDB/Redux/JWT]",
-        imgSrc:"/cbxworld.png",
-        siteUrl: "https://cbxworld.vercel.app/",
-        github:"https://github.com/w-michelle/cbxworld"
+        title:"CYBER BUBBLE",
+        desc: "First project, inspired by imissmybar with added organizational tools",
+        stacks: "[React/TailwindCSS/Firebase]",
+        imgSrc: "/cb-home.png",
+        siteUrl: "https://cyberbubblev2.vercel.app",
+        github: "https://github.com/w-michelle/cyberbubblev2"
+
     },
+ 
  
 ]
   
@@ -63,10 +65,10 @@ const Projects = () => {
                 projectInfo.map((project) => (
                 <div key={project.title} className="my-[8rem]">
                     <div className="flex gap-4 flex-wrap md:flex-nowrap md:justify-between">
-                     <div className="flex flex-col gap-3"> 
-                        <p className="text-xs break-words w-[310px] tracking-widest">{project.stacks}</p>
+                        <div className="flex flex-col gap-3"> 
+                        <p className="text-xs break-words w-[340px] tracking-widest">{project.stacks}</p>
                         <h3 className="tracking-wider">{project.title}</h3>
-                        <p className="text-xs w-[250px] text-neutral-400">{project.desc}</p>
+                        <p className="text-xs w-[270px] text-neutral-400">{project.desc}</p>
                         <Link href={project.siteUrl} aria-label="view project" target="_blank">
                               <button className=" text-xs w-[100px] py-2 px-4 bg-[#C6F6D5] text-[#22543D] cursor-pointer hover:bg-[#22543D] hover:text-[#C6F6D5] transition">
                                  VIEW SITE
@@ -77,7 +79,7 @@ const Projects = () => {
                             GITHUB
                             </button>
                         </Link>
-                     </div>
+                        </div>
                       
                         <Link href={project.siteUrl} aria-label="view project" target="_blank" className="relative w-[300px] h-[200px] md:w-[380px] md:h-[250px] mr-5">
                             <Image src={project.imgSrc} fill alt="Project Image" className="object-cover object-top shadow-custom rounded-md" />
